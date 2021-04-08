3eefdcd4498edfe9f451ec696c5ba95c856243e1
Mattis Erkensten <m.erkensten@icloud.com>
2021-04-08 14:06:24 +0200

    Mobil menu påbörjad

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-function myFunction(x) {
-    x.classList.toggle("change");
+// Funktion för mobilmenyn
+function myFunction() {
+  const x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+} 
